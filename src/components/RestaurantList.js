51e8ab334692b38d61af17e8052858c6eadcfc82
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-05-21 16:16:31 -0400

    my-first-commit

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { Col, Row, Card } from "antd";
+
+function RestaurantList() {
+  const [restaurants, setRestaurants] = useState();
+  useEffect(() => {
+    fetch(`https://my-first-firestore-jr.web.app/restaurants`)
+      .then((response) => response.json())
+      .then((data) => setRestaurants(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <section>
+      <Row gutter={16}>
+        {!restaurants ? (
+          <h2> Loading ... </h2>
+        ) : (
+          restaurants.map((restaurant) => (
+            <Col style={{ width: "300px" }} key={restaurant.id}>
+              <Card title={restaurant.name} hoverable>
+                <p>{restaurant.address} </p>
+              </Card>
+            </Col>
+          ))
+        )}
+      </Row>
+    </section>
+  );
+}
+
+export default RestaurantList;
